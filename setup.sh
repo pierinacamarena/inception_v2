e9a5a782814c21ee8791f5f60fb3867aa2c9ad9f
PierinaCamarena <pcamaren@student.42.fr>
2023-02-08 22:04:49 +0100

    added script to install docker and docker-compose

---
+++ setup.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+#install docker
+
+sudo apt update
+sudo apt install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+sudo apt update
+sudo apt install -y docker-ce docker-ce-cli containerd.io
+sudo docker run hello-world
+
+#install docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.28.6/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
+
