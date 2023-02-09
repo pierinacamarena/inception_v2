9569e080aa700c2c82b30e4b643f9e064cfcd037
PierinaCamarena <pcamaren@student.42.fr>
2023-02-09 18:54:00 +0100

    mariadb running, but msql permission denied

---
+++ srcs/requirements/mariadb/tools/auto_config.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+DIRECTORY_DATABASE = /var/lib/mysql/wordpress
+
+if [! -d $DIRECTORY_DATABASE]; then
+	/usr/bin/mysqld_safe --datadir=/var/lib/mysql &
+	until mysqladmin ping &>/dev/null; do
+		sleep 1
+	done
+
+	mysql -u root << EOF
+	
+	CREATE DATABASE IF NOT EXISTS${MYSQL_DATABASE};
+
+	ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
+
+	DELETE FROM mysql.user WHERE user='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+	DELETE FROM mysql.user WHERE user='';
+
+	CREATE USER '${MYSQL_USER}'@'%'IDENTIFIED BY '${MYSQL_PASSWORD}';
+	GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
+	
+	FLUSH PRIVILEGES;
+
+EOF
+
+	killall mysqld 2> /dev/null
+fi
+
+
+exec"$@"
