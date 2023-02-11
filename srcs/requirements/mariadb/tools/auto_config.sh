1728c9527a336d5ab1ceacf0f9d3a6c582643163
PierinaCamarena <pcamaren@student.42.fr>
2023-02-11 17:10:40 +0100

    script: fixed space

--- srcs/requirements/mariadb/tools/auto_config.sh
+++ srcs/requirements/mariadb/tools/auto_config.sh
@@ -1,16 +1,38 @@
 #!/bin/bash
 
-DIRECTORY_DATABASE = /var/lib/mysql/wordpress
+set -eux
 
-if [! -d $DIRECTORY_DATABASE]; then
-	/usr/bin/mysqld_safe --datadir=/var/lib/mysql &
+DIRECTORY_DATABASE=/var/lib/mysql/wordpress
+
+if [ ! -d "$DIRECTORY_DATABASE" ]; then
+		/usr/bin/mysqld_safe --datadir=/var/lib/mysql &
 	until mysqladmin ping &>/dev/null; do
 		sleep 1
 	done
 
+	#mysql command is a client for connecting and communicating with the
+	#the MYSQL database.
+	#-u root specifies that the client should connect as the root user,
+	#which has admin privileges
+
+	#ALTER USER is used to modify the properties of an existing user
+	#in this case we are setting the password for the root user of the MYSQL instance
+	
+	#DELETE FROM lines are for security, the first one deletes all users from the MYSQL
+	#instance except for the root
+	#removes all root users that can connect from hosts other than localhost, 127.0.0.1, or ::1.
+	#remove also all users where the user column is an empty string
+
+	#then we create a a new user, and specify its password, this user can connect from any machine
+	#on the network
+	#we grant all privileges, meaning the user will be able to have full access to the database
+	#and perform any action
+
+	#flushing the privileges allows to reload the MSQL server's grant tables and update the in-memory
+	#privileges with any changes made.
 	mysql -u root << EOF
 	
-	CREATE DATABASE IF NOT EXISTS${MYSQL_DATABASE};
+	CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
 
 	ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
 
@@ -23,9 +45,8 @@ if [! -d $DIRECTORY_DATABASE]; then
 	FLUSH PRIVILEGES;
 
 EOF
-
 	killall mysqld 2> /dev/null
 fi
 
 
-exec"$@"
+exec "$@"
